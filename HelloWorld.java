e00999be659aae7648943d3ed7d3a7fdbc8a51ef
root <root@ip-172-31-45-159.ec2.internal>
2023-11-08 08:31:15 +0000

    second

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-    System.out.println("hello");
+    System.out.println("How are you doing ");
     System.out.println("good morning");
     System.out.println("welcome on board");
   }
